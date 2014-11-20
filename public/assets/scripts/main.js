20fe92c3f3787eebeaec2d51691f76390beb6939
Leah de Vries <dustingamble@Dustins-MacBook-Pro.local>
2014-11-20 00:33:22 -0500

    broke everything

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -4,5 +4,16 @@ jQuery(function($) {
     
     // Create New Socket Connection using Socket.io
     var socket = io();
+    io.on('connection', function(socket){
+	  socket.on('chat message', function(msg){
+	    console.log('message: ' + msg);
+	  });
+	});
     
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
